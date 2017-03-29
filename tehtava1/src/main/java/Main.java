25e2b09ed0fa41a41652afcb69c516589444ed8f
Kalle Perkoila <kalle.perkoila@helsinki.fi>
2017-03-29 10:31:17 +0300

    tehtävä 2

--- tehtava1/src/main/java/Main.java
+++ tehtava1/src/main/java/Main.java
@@ -4,7 +4,8 @@ import ohtu.Multiplier;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Multiplier kolme = new Multiplier(3);
+        final int parametri = 3;
+        Multiplier kolme = new Multiplier(parametri);
         System.out.println("anna luku ");
         int luku = scanner.nextInt();
 
